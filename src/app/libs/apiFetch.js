78e54ae8b7d3f86c3eee50bb4840edeb1ed0cafc
Salvador511 <salvadororozcogon511@gmail.com>
2024-11-30 08:30:39 -0600

    libs to fetch

---
+++ src/app/libs/apiFetch.js
@@ -0,0 +1,23 @@
+
+const apiFetch = async ({ payload, method, url }) => {
+  try {
+    const token = localStorage.getItem('token')
+    const options = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    if (payload) {
+      options.body = JSON.stringify(payload)
+    }
+    const response = await fetch(url, options)
+    return response.json()
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+export default apiFetch
